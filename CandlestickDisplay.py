ba398115e6b8f9f77f1494b4b50be826d433b1c0
Joey Schmidt <49414795+DaSunnySide@users.noreply.github.com>
2019-09-07 08:40:29 -0700

    Update CandlestickDisplay.py

--- CandlestickDisplay.py
+++ CandlestickDisplay.py
@@ -1,17 +1,8 @@
-import plotly.graph_objs as go
 import pandas as pd
-import plotly.graph_objs as go
+import plotly.graph_objects as go
 import plotly.offline as pyo
 from pandas.io.json import json_normalize
 import plotly.tools as tools
-import dash
-import dash
-import dash_html_components as html
-import dash_core_components as dcc
-from dash.dependencies import Input, Output, State
-
-#Initialize App
-app = dash.Dash()
 
 #Define Colors for the UI
 colors = {
@@ -75,76 +66,17 @@ graph_options=[
             {'label': 'Time Series Monthly', 'value' : 'TIME_SERIES_MONTHLY'},
             {'label': 'Time Series Monthly Adjusted', 'value' : 'TIME_SERIES_MONTHLY_ADJUSTED'}
         ]
-app.layout = html.Div(([
-    html.Div([
-        dcc.Dropdown(
-        id='stock-symbol',
-        options=options,
-        value='MSFT',
-        multi=True)
-        ], style={'display': 'flex', 'verticalAlign':'top', 'width':'30%'}),
-
-    html.Div([
-        dcc.Dropdown(
-            id='graph-type',
-            options=graph_options,
-            value='TIME_SERIES_DAILY'
-    )
-    ], style={'display': 'flex', 'verticalAlign':'top', 'width':'30%'}),
-
-    html.Div([
-        html.Button(
-            id='submit-button',
-            children='Submit',
-            n_clicks=0,
-            style={'fontSize':24, 'marginLeft':'30px'}
-        )
-    ], style={'display': 'flex', 'verticalAlign': 'top', 'width': '30%'}),
-
-    html.Div([
-        dcc.Graph(id='linegraph',
-                  figure={
-                      'data': [
-                          go.Scatter(x=df['timestamp'], y=df['close'],
-                                     mode='lines', marker={
-                                  'size': 12,
-                                  'color': '#4dff4d'
-                              })],
-                      'layout': go.Layout(title='Stock prices over time')}),
-    ]),
-
-    html.Div([
-        dcc.Graph(id='candlestick',
-                      figure={
-                          'data': [
-                              go.Candlestick(x=df['timestamp'],
-                                             open=df['open'],
-                                             high=df['high'],
-                                             low=df['low'],
-                                             close=df['close'],
-                                             increasing_line_color='#4DFF4D',
-                                             decreasing_line_color='#193EFD'
-                                             )
-                          ],
-                            'layout': go.Layout(title='Candlestick Graph of prices over time')
-                      })
-
-    ])
-]
-))
-@app.callback(Output('linegraph', 'figure'),
-              [Input('submit-button', 'n_clicks')],
-              [State('stock-symbol', 'value'),
-               State('graph-type', 'value')])
-
-def update_figure(n_clicks, selected_graph_type, selected_stock_symbol):
-    function = selected_graph_type
-    symbol = selected_stock_symbol
-    API_QUERY = 'https://www.alphavantage.co/query?function=' + function + '&symbol=' + symbol + '&' + 'apikey=' + API_KEY + '&' + datatype
-    df1 = pd.read_csv(API_QUERY)
-    return {'data': df1, 'layout': go.Layout(title='Stock prices over time')}
+fig = go.Figure(data=[go.Candlestick(x=df['timestamp'],
+                                     open=df['open'],
+                                     high=df['high'],
+                                     low=df['low'],
+                                     close=df['close'],
+                                     increasing_line_color='#4DFF4D',
+                                     decreasing_line_color='#193EFD'
+                                     )
+              ])
 
 
 
 if __name__ == '__main__':
-    app.run_server()
+    fig.show()
